532e58f2f1fc2232cfb44211931f72c951c22147
WonderSi <kirillkozlov00@gmail.com>
2026-02-26 11:34:29 +0700

    feat: init backend

---
+++ backend/src/main/kotlin/com/example/cardio/CardioApplication.kt
@@ -0,0 +1,11 @@
+package com.example.cardio
+
+import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.boot.runApplication
+
+@SpringBootApplication
+class CardioApplication
+
+fun main(args: Array<String>) {
+	runApplication<CardioApplication>(*args)
+}
---
+++ backend/src/test/kotlin/com/example/cardio/CardioApplicationTests.kt
@@ -0,0 +1,13 @@
+package com.example.cardio
+
+import org.junit.jupiter.api.Test
+import org.springframework.boot.test.context.SpringBootTest
+
+@SpringBootTest
+class CardioApplicationTests {
+
+	@Test
+	fun contextLoads() {
+	}
+
+}
