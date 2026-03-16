dddd34adbd03e0637a962188f034be134e3c3280
WonderSi <kirillkozlov00@gmail.com>
2026-03-16 19:44:09 +0700

    feat: update structure

--- backend/src/main/kotlin/com/medical/cardio/CardioApplication.kt
+++ backend/src/main/kotlin/com/medical/cardio/CardioApplication.kt
@@ -1,4 +1,4 @@
-package com.example.cardio
+package com.medical.cardio
 
 import org.springframework.boot.autoconfigure.SpringBootApplication
 import org.springframework.boot.runApplication
--- backend/src/test/kotlin/com/medical/cardio/CardioApplicationTests.kt
+++ backend/src/test/kotlin/com/medical/cardio/CardioApplicationTests.kt
@@ -1,4 +1,4 @@
-package com.example.cardio
+package com.medical.cardio
 
 import org.junit.jupiter.api.Test
 
