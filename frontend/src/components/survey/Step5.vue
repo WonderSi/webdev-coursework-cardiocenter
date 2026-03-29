92e18a11086896b3ba0ed183509418dcffe4cebd
GreyF <greysforgettea@gmail.com>
2026-03-29 20:50:12 +0700

    feat: step5 added, fix: namings

---
+++ frontend/src/components/survey/Step5.vue
@@ -0,0 +1,91 @@
+<template>
+  <div class="form-container">
+    <div class="form-group">
+      <label class="label">Глюкоза:</label>
+      <div class="input-group">
+        <input type="number" class="input-field" placeholder="4,5" v-model="form.glucose" />
+        <span class="unit">ммоль/л</span>
+      </div>
+    </div>
+    
+    <div class="form-group">
+      <label class="label">Холестерин:</label>
+      <div class="input-group">
+        <input type="number" class="input-field" placeholder="5,3" v-model="form.cholesterol" />
+        <span class="unit">ммоль/л</span>
+      </div>
+    </div>
+
+    <div class="form-group">
+      <label class="label">Холестерин не-ЛПВП:</label>
+      <div class="input-group">
+        <input type="number" class="input-field" placeholder="3,2" v-model="form.nonHdlCholesterol" />
+        <span class="unit">ммоль/л</span>
+      </div>
+    </div>
+
+    <div class="form-group">
+      <label class="label">Холестерин ЛПОНП:</label>
+      <div class="input-group">
+        <input type="number" class="input-field" placeholder="0,8" v-model="form.vldlCholesterol" />
+        <span class="unit">ммоль/л</span>
+      </div>
+    </div>
+
+    <div class="form-group">
+      <label class="label">Холестерин ЛПВП:</label>
+      <div class="input-group">
+        <input type="number" class="input-field" placeholder="1,2" v-model="form.hdlCholesterol" />
+        <span class="unit">ммоль/л</span>
+      </div>
+    </div>
+
+    <div class="form-group">
+      <label class="label">Холестерин ЛПНП:</label>
+      <div class="input-group">
+        <input type="number" class="input-field" placeholder="3,0" v-model="form.ldlCholesterol" />
+        <span class="unit">ммоль/л</span>
+      </div>
+    </div>
+
+    <div class="form-group">
+      <label class="label">Аполипопротеин А (Апо А):</label>
+      <div class="input-group">
+        <input type="number" class="input-field" placeholder="1,5" v-model="form.apolipoproteinA" />
+        <span class="unit">г/л</span>
+      </div>
+    </div>
+
+    <div class="form-group">
+      <label class="label">Аполипопротеин B (Апо B):</label>
+      <div class="input-group">
+        <input type="number" class="input-field" placeholder="0,9" v-model="form.apolipoproteinB" />
+        <span class="unit">г/л</span>
+      </div>
+    </div>
+
+    <div class="form-group">
+      <label class="label">Триглицериды:</label>
+      <div class="input-group">
+        <input type="number" class="input-field" placeholder="1,7" v-model="form.triglycerides" />
+        <span class="unit">ммоль/л</span>
+      </div>
+    </div>
+  </div>
+</template>
+
+<script setup lang="ts">
+defineProps<{
+  form: Record<string, any>
+}>()
+</script>
+
+<style scoped lang="scss">
+.form-container {
+  gap: 24px;
+}
+
+.label {
+  width: 220px;
+}
+</style>
--- frontend/src/pages/SurveyPage.vue
+++ frontend/src/pages/SurveyPage.vue
@@ -51,21 +51,26 @@ import Step1 from '@/components/survey/Step1.vue'
 import Step2 from '@/components/survey/Step2.vue'
 import Step3 from '@/components/survey/Step3.vue'
 import Step4 from '@/components/survey/Step4.vue'
+import Step5 from '@/components/survey/Step5.vue'
 
-const stepComponents = [Step1, Step2, Step3, Step4]
+const stepComponents = [Step1, Step2, Step3, Step4, Step5]
 
 const form = ref<Record<string, any>>({
   gender: null, age: null, height: null, weight: null, hipMeasurement: null,
   alcohol: null,
   profession: null,
-  region: null
+  region: null,
+  glucose: null, cholesterol: null, nonHdlCholesterol: null, vldlCholesterol: null,
+    hdlCholesterol: null, ldlCholesterol: null, apolipoproteinA: null, apolipoproteinB: null,
+    triglycerides: null
 })
 
 const stepsMeta = ref([
   { id: 1, title: 'Укажите, пожалуйста, ваши основные параметры:', requiredFields: ['gender', 'age', 'height', 'weight'] },
   { id: 2, title: 'Употребляете ли вы алкоголь?', requiredFields: ['alcohol'] },
   { id: 3, title: 'Выберите ваш род деятельности', requiredFields: ['profession'] },
-  { id: 4, title: 'Где вы проживаете', requiredFields: ['region'] }
+  { id: 4, title: 'Где вы проживаете', requiredFields: ['region'] },
+  { id: 5, title: 'Ваши лабораторные параметры', requiredFields: [] }
 ])
 
 const currentStepIndex = ref(0)
