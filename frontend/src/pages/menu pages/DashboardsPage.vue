b4af636ccbcdc826292fd870eb65e69b23fd7890
GreyF <greysforgettea@gmail.com>
2026-04-22 15:55:02 +0700

    feat: first ver of template; added one lib for charts

--- frontend/src/pages/menu pages/DashboardsPage.vue
+++ frontend/src/pages/menu pages/DashboardsPage.vue
@@ -1,5 +1,43 @@
 <template>
   <sidebar/>
+  <div class="dashboards-grid">
+
+    <div class="small-card">
+      <div class="small-card-header">
+        <div class="small-card-icon"></div>
+        <div class="small-card-header"></div>
+      </div>
+      <span class="small-card-number">12352</span>
+      <div class="last-update">
+        <div class="dynamics">
+          <div class="dynamics-icon"></div>
+          <span class="dynamics-span">+100</span>
+        </div>
+        <span class="last-update">02.04.2026</span>
+      </div>
+    </div>
+
+    <div class="small-card">
+      <div class="small-card-header">
+        <div class="small-card-icon"></div>
+        <div class="small-card-header"></div>
+      </div>
+      <div class="small-card-two-numbers">
+        <div class="percentage">
+          <span class="small-card-number">58%</span>
+          <span class="men">мужчины</span>
+        </div>
+        <div class="percentage">
+          <span class="small-card-number">42%</span>
+          <span class="men">женщины</span>
+        </div>
+      </div>
+    </div>
+
+
+  </div>
+
+
   
 </template>
 
