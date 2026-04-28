a71dcdc4d842fb02abd482129d359073552fbe3e
WonderSi <kirillkozlov00@gmail.com>
2026-04-28 22:13:47 +0700

    fix: add import { useUserStore }

--- frontend/src/main.ts
+++ frontend/src/main.ts
@@ -1,5 +1,6 @@
 import { createApp } from 'vue'
 import { createPinia } from 'pinia'
+import { useUserStore } from '@/stores/user.store'
 import router from '@/router/index'
 import '@/styles/main.scss'
 import App from './App.vue'
