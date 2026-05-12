409d94e1bcf7a372a9ae442e30308912df32a8c3
GreyF <greysforgettea@gmail.com>
2026-05-12 10:49:43 +0700

    fix: grid done

--- frontend/src/pages/menu pages/DashboardsPage.vue
+++ frontend/src/pages/menu pages/DashboardsPage.vue
@@ -2,39 +2,37 @@
   <sidebar/>
   <div class="dashboards-grid-container">
 
-    <!-- ================= СТРОКА 1 (Маленькие карточки, Высота 1) ================= -->
-
-    <!-- Карточка: Всего записей -->
-    <div class="dashboard-card col-1 row-1">
-      <div class="card-header">
-        <div class="card-icon">
-          <svg width="40" height="40" viewBox="0 0 40 40" fill="none" xmlns="http://www.w3.org/2000/svg">
-            <path d="M18.3333 26.6667V35H8.33333C7.49237 35.0003 6.68239 34.6827 6.06575 34.1108C5.44912 33.539 5.07141 32.7553 5.00833 31.9167L5 31.6667V26.6667H18.3333ZM35 26.6667V31.6667C35.0003 32.5076 34.6827 33.3176 34.1108 33.9342C33.539 34.5509 32.7553 34.9286 31.9167 34.9917L31.6667 35H21.6667V26.6667H35ZM18.3333 16.6667V23.3333H5V16.6667H18.3333ZM35 16.6667V23.3333H21.6667V16.6667H35ZM18.3333 5V13.3333H5V8.33333C4.99973 7.49237 5.31734 6.68239 5.88916 6.06575C6.46098 5.44912 7.24474 5.07141 8.08333 5.00833L8.33333 5H18.3333ZM31.6667 5C32.5507 5 33.3986 5.35119 34.0237 5.97631C34.6488 6.60143 35 7.44928 35 8.33333V13.3333H21.6667V5H31.6667Z" fill="#0f356e"/>
-          </svg>
+    <div class="kpi-column col-1 row-2">
+      <div class="dashboard-card kpi-card">
+        <div class="card-header">
+          <div class="card-icon">
+            <svg width="40" height="40" viewBox="0 0 40 40" fill="none" xmlns="http://www.w3.org/2000/svg">
+              <path d="M18.3333 26.6667V35H8.33333C7.49237 35.0003 6.68239 34.6827 6.06575 34.1108C5.44912 33.539 5.07141 32.7553 5.00833 31.9167L5 31.6667V26.6667H18.3333ZM35 26.6667V31.6667C35.0003 32.5076 34.6827 33.3176 34.1108 33.9342C33.539 34.5509 32.7553 34.9286 31.9167 34.9917L31.6667 35H21.6667V26.6667H35ZM18.3333 16.6667V23.3333H5V16.6667H18.3333ZM35 16.6667V23.3333H21.6667V16.6667H35ZM18.3333 5V13.3333H5V8.33333C4.99973 7.49237 5.31734 6.68239 5.88916 6.06575C6.46098 5.44912 7.24474 5.07141 8.08333 5.00833L8.33333 5H18.3333ZM31.6667 5C32.5507 5 33.3986 5.35119 34.0237 5.97631C34.6488 6.60143 35 7.44928 35 8.33333V13.3333H21.6667V5H31.6667Z" fill="#0f356e"/>
+            </svg>
+          </div>
+          <div class="card-title">Всего записей о пациентах</div>
         </div>
-        <div class="card-title">Всего записей о пациентах</div>
+        <span class="card-number-total-records">{{ mockKpiData.totalRecords }}</span>
       </div>
-      <span class="card-number-total-records">{{ mockKpiData.totalRecords }}</span>
-    </div>
 
-    <!-- Карточка: % диагностированных м-ж -->
-    <div class="dashboard-card col-1 row-1">
-      <div class="card-header">
-        <div class="card-icon">
-          <svg width="40" height="40" viewBox="0 0 24 24" fill="none" xmlns="http://www.w3.org/2000/svg">
-            <path d="M12 21.35l-1.45-1.32C5.4 15.36 2 12.28 2 8.5 2 5.42 4.42 3 7.5 3c1.74 0 3.41.81 4.5 2.09C13.09 3.81 14.76 3 16.5 3 19.58 3 22 5.42 22 8.5c0 3.78-3.4 6.86-8.55 11.54L12 21.35z" fill="#0f356e"/>
-          </svg>
+      <div class="dashboard-card kpi-card">
+        <div class="card-header">
+          <div class="card-icon">
+            <svg width="40" height="40" viewBox="0 0 24 24" fill="none" xmlns="http://www.w3.org/2000/svg">
+              <path d="M12 21.35l-1.45-1.32C5.4 15.36 2 12.28 2 8.5 2 5.42 4.42 3 7.5 3c1.74 0 3.41.81 4.5 2.09C13.09 3.81 14.76 3 16.5 3 19.58 3 22 5.42 22 8.5c0 3.78-3.4 6.86-8.55 11.54L12 21.35z" fill="#0f356e"/>
+            </svg>
+          </div>
+          <div class="card-title">Пациентов с ССЗ</div>
         </div>
-        <div class="card-title">Пациентов с ССЗ</div>
-      </div>
-      <div class="card-two-numbers">
-        <div class="percentage">
-          <span class="card-number color-blue">{{ mockGenderDiagnosesData[0].malePercent }}%</span>
-          <span class="sub-label color-blue">{{ mockGenderDiagnosesData[0].maleDiagnosed }} чел (М)</span>
-        </div>
-        <div class="percentage">
-          <span class="card-number color-pink">{{ mockGenderDiagnosesData[1].femalePercent }}%</span>
-          <span class="sub-label color-pink">{{ mockGenderDiagnosesData[1].femaleDiagnosed }} чел (Ж)</span>
+        <div class="card-two-numbers">
+          <div class="percentage">
+            <span class="card-number color-blue">{{ mockGenderDiagnosesData[0].malePercent }}%</span>
+            <span class="sub-label color-blue">{{ mockGenderDiagnosesData[0].maleDiagnosed }} чел (М)</span>
+          </div>
+          <div class="percentage">
+            <span class="card-number color-pink">{{ mockGenderDiagnosesData[1].femalePercent }}%</span>
+            <span class="sub-label color-pink">{{ mockGenderDiagnosesData[1].femaleDiagnosed }} чел (Ж)</span>
+          </div>
         </div>
       </div>
     </div>
@@ -674,6 +672,18 @@ const chartDistricts = computed(() => {
 // -----------------------------------------------------------------
 
 // --- СТИЛИ ДЛЯ МАЛЕНЬКИХ ВЕРХНИХ КАРТОЧЕК
+
+// --- ОБЕРТКА ДЛЯ ДВУХ ВЕРХНИХ ---
+.kpi-column {
+  display: flex;
+  flex-direction: column;
+  gap: 24px;
+}
+
+.kpi-card {
+  flex: 1;
+}
+
 .card-header {
   display: flex;
   align-items: center;
@@ -794,7 +804,7 @@ const chartDistricts = computed(() => {
     display: flex;
     align-items: center;
     gap: 6px;
-    font-size: 1rem;
+    font-size: 1.2rem;
     font-weight: 500;
     color: $color-blue-dark;
   }
